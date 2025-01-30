707b980338845737d59f33c1341a201630962f05
Dulyarat455 <dulyarat.455@gmail.com>
2025-01-30 17:00:27 +0700

    edit logic filter path

--- frontend/src/components/slidebarSetting/SlidebarSettingPage.tsx
+++ frontend/src/components/slidebarSetting/SlidebarSettingPage.tsx
@@ -15,11 +15,11 @@ const SlidebarSettingPage: React.FC = () => {
                 onMouseLeave={() => setShowDiv(true)}
                 className="mt-6 ml-48 h-[42px]  items-center inline-flex gap-14 ">
 
-                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean).pop() || "")} pathName={"access"} showDiv={showDiv} name={"Access"}/>
-                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean).pop() || "")} pathName={"graphs"} showDiv={showDiv} name={"Graphs"}/>
-                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean).pop() || "")} pathName={"powers"} showDiv={showDiv} name={"Powers"}/>
-                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean).pop() || "")} pathName={"networking"} showDiv={showDiv} name={"Networking"}/>
-                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean).pop() || "")} pathName={"destroy"} showDiv={showDiv} name={"Destroy"}/>
+                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean)[1] || "")} pathName={"access"} showDiv={showDiv} name={"Access"}/>
+                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean)[1] || "")} pathName={"graphs"} showDiv={showDiv} name={"Graphs"}/>
+                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean)[1] || "")} pathName={"powers"} showDiv={showDiv} name={"Powers"}/>
+                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean)[1] || "")} pathName={"networking"} showDiv={showDiv} name={"Networking"}/>
+                <ContainerSlidebar currentPath={(location.pathname.split("/").filter(Boolean)[1] || "")} pathName={"destroy"} showDiv={showDiv} name={"Destroy"}/>
             </div>
             <div className="absolute shadow-md top-36 ml-2 mt-2 px-[50%] py-1 bg-red-300 bg-opacity-50 z-[-1] rounded-full"></div>
         </nav>
