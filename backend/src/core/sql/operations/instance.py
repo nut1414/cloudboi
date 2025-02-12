3e80d1cb2dd37dc84cbf4bee345adf98c85ba638
jinnaWan <jinna.wnsp@gmail.com>
2025-02-12 11:27:39 +0700

    Add type check to sql operation

--- backend/src/core/sql/operations/instance.py
+++ backend/src/core/sql/operations/instance.py
@@ -1,3 +1,4 @@
+from typing import List
 from .base import BaseOperation
 from ..tables.instance_type import InstanceType
 from ..tables.os_type import OsType
@@ -6,11 +7,11 @@ from sqlalchemy import select
 
 
 class InstanceOperation(BaseOperation):
-    async def get_all_instance_types(self):
+    async def get_all_instance_types(self) -> List[InstanceTypeModel]:
         stmt = select(InstanceType)
         result = (await self.db.execute(stmt)).scalars().all()
         return self.to_pydantic(InstanceTypeModel, result)
-    async def get_all_os_types(self):
+    async def get_all_os_types(self) -> List[OsTypeModel]:
         stmt = select(OsType)
         result = (await self.db.execute(stmt)).scalars().all()
         return self.to_pydantic(OsTypeModel, result)
