32806a8a6ab481e7b2f1dc65875c238f97467c8d
Dulyarat455 <138790626+Dulyarat455@users.noreply.github.com>
2025-03-30 00:01:52 +0700

    new design slidebar  for InstanceSetting Page

--- frontend/src/components/Instance/Setting/SlidebarSetting/SlidebarSetting.tsx
+++ frontend/src/components/Instance/Setting/SlidebarSetting/SlidebarSetting.tsx
@@ -16,15 +16,14 @@ const SlidebarSetting: React.FC<SlidebarSettingProps> = ({active,setActive}) => 
         <nav >
             <div onMouseEnter={() => setShowDiv(false)}
                 onMouseLeave={() => setShowDiv(true)}
-                className="mt-6 ml-48 h-[42px]  items-center inline-flex gap-14 ">
+                className="mt-6 ml-56 h-[42px]  items-center inline-flex gap-14 ">
 
                 <ContainerSlidebar active={active} setActive={ setActive}  menu={"AccessMenu"} showDiv={showDiv} name={"Access"}/>
-                <ContainerSlidebar active={active} setActive={ setActive}  menu={"GraphsMenu"} showDiv={showDiv} name={"Graphs"}/>
                 <ContainerSlidebar active={active} setActive={ setActive}  menu={"PowersMenu"} showDiv={showDiv} name={"Powers"}/>
                 <ContainerSlidebar active={active} setActive={ setActive}  menu={"NetworkingMenu"} showDiv={showDiv} name={"Networking"}/>
                 <ContainerSlidebar active={active} setActive={ setActive}  menu={"DestroyMenu"} showDiv={showDiv} name={"Destroy"}/>
             </div>
-            <div className="absolute shadow-md top-36 ml-2 mt-2 px-[50%] py-1 bg-red-300 bg-opacity-50 z-[-1] rounded-full"></div>
+            <div className="absolute shadow-md top-36 ml-2 mt-2 px-[50%] py-1 bg-[#192A51] bg-opacity-50 z-[-1] rounded-full"></div>
         </nav>
     );
 };
