171871150a06397817b2f17f75afc791d961e719
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2025-04-10 16:59:53 +0700

    add permission check on lxd_cluster

--- backend/src/core/service/lxd_cluster.py
+++ backend/src/core/service/lxd_cluster.py
@@ -1,5 +1,3 @@
-from typing import List
-from .clients.base_instance_client import BaseInstanceClient
 from .clients.lxd import LXDClient
 from fastapi import Depends
 from ..models.lxd_cluster import (
@@ -7,6 +5,8 @@ from ..models.lxd_cluster import (
     AddMemberResponse, AddMemberRequest, 
     GetClusterMembersStateInfoResponse
 )
+from ..utils.permission import require_roles
+from ..constants.user_const import UserRole
 
 
 class LXDClusterService:
@@ -16,14 +16,17 @@ class LXDClusterService:
     ):
         self.lxd_client = lxd_client
         
+    @require_roles([UserRole.ADMIN])
     async def create_lxd_cluster_join_token(self, request: CreateJoinTokenRequest) -> CreateJoinTokenResponse:
         token = self.lxd_client.create_lxd_cluster_join_token(request.server_name)
         return CreateJoinTokenResponse(join_token=token)
       
+    @require_roles([UserRole.ADMIN])
     async def add_member_to_lxd_cluster_group(self, request: AddMemberRequest) -> AddMemberResponse:
         add_result = self.lxd_client.add_member_to_lxd_cluster_group(request.server_name)
         return AddMemberResponse(success=add_result)
       
+    @require_roles([UserRole.ADMIN])
     async def get_lxd_cluster_members_state_info(self) -> GetClusterMembersStateInfoResponse:
         members = self.lxd_client.get_lxd_cluster_members()
         member_states = []
