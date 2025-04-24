6ff58333ddf0ee6495324bc6656a70124f91e44d
jinnaWan <jinna.wnsp@gmail.com>
2025-04-24 21:55:30 +0700

    fix env variable for dev mode

--- backend/src/infra/config.py
+++ backend/src/infra/config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 CERT_DIR = os.path.join(BASE_DIR, 'backend', 'test_certificate', 'tmp')
 
 class LXDConfig:
-    LXD_HOST = '192.168.1.227:8443' if APP_ENV == 'test' else os.environ.get('LXD_HOST', '192.168.1.230:8443')
+    LXD_HOST = '192.168.1.227:8443' if APP_ENV == 'test' else os.environ.get('LXD_HOST') or '192.168.1.230:8443'
     LXD_CERT = (
         os.environ.get('LXD_CERT_PATH', os.path.join(CERT_DIR, 'lxd.crt')),
         os.environ.get('LXD_KEY_PATH', os.path.join(CERT_DIR, 'lxd.key')),
